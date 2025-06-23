7a438e56141aadcdecf39eda8f3420f0551a2f69
SujataSaundale07 <s.saundale16@gmail.com>
2025-06-23 05:50:59 -0700

    Scanner demo

---
+++ ScannerDemo.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class ScannerDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // interger Using nextInt()
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        //withoiut sc.nextLine() it will works fine
+        //sc.nextLine();
+
+        //string  Using next()
+        System.out.print("Enter your first name: ");
+        String firstName = sc.next();
+
+        // sc.nextLine()is not given then nextLine() may just read the leftover newline and return an empty string.
+        //sc.nextLine();
+
+        // string Using nextLine()
+        System.out.print("Enter your full address: ");
+        String address = sc.nextLine();
+
+       
+        System.out.println("Collected Info");
+        System.out.println("Age: " + age);
+        System.out.println("First Name: " + firstName);
+        System.out.println("Full Address: " + address);
+
+       
+    }
+}
+
