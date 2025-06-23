29c90b2488830e05b3b0f68190df1d15ed5f6ec1
NCoder-Java <NCoder-Java@github.com>
2025-06-23 10:43:41 +0530

    Use of display Methods - Print & Println with example

---
+++ DisplayMethod.java
@@ -0,0 +1,13 @@
+// println and print method Use with Example
+
+public class PrintStm
+{
+	public static void main(String[] args) {
+	    
+	    System.out.println("Hi");
+	    System.out.print("Welcome ");
+            System.out.println("to java");
+		
+	}
+}
+
